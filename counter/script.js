eef5dd4ef01959c07ae131597803d33c0f47bad7
Haykuhie <haykuhiek@gmail.com>
2021-12-01 18:31:30 +0400

    local storage bug fixed

--- counter/script.js
+++ counter/script.js
@@ -6,19 +6,22 @@ const decreaseBtn = document.querySelector(".decrease");
 const increaseBtn = document.querySelector(".increase");
 const resetBtn = document.querySelector(".reset");
 
-let number = getLocalStorageData("counter");
-counter.innerText = number;
+let localStorageData = getLocalStorageData("counter");
+counter.innerText = localStorageData
+if(localStorageData>0)decreaseBtn.style.cursor = "pointer";
+
+
 decreaseBtn.addEventListener("click", () => {
   let localStorageData = getLocalStorageData("counter");
-
   if (+localStorageData <= 0) {
     decreaseBtn.style.cursor = "not-allowed";
     return;
-  }
-  localStorageData -= 1;
+  }  
   counter.innerText = +counter.innerText - 1;
+  localStorageData -= 1;  
   setLocalStorageData("counter", localStorageData);
 });
+
 increaseBtn.addEventListener("click", () => {
   let localStorageData = getLocalStorageData("counter");
   if (localStorageData >= 0) decreaseBtn.style.cursor = "pointer";
@@ -26,6 +29,7 @@ increaseBtn.addEventListener("click", () => {
   counter.innerText = +counter.innerText + 1;
   setLocalStorageData("counter", localStorageData);
 });
+
 resetBtn.addEventListener("click", () => {
   counter.innerText = 0;
   setLocalStorageData("counter", 0);
