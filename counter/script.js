8208c9bae4a421dd998ebd4090ebd2b332bac842
Haykuhie <haykuhiek@gmail.com>
2021-12-01 15:42:45 +0400

    full functionality of buttons added

--- counter/script.js
+++ counter/script.js
@@ -1,15 +1,19 @@
-const counter = document.querySelector('p')
-const decreaseBtn = document.querySelector('.decrease') 
-const increaseBtn = document.querySelector('.increase')   
-const resetBtn = document.querySelector('.reset')     
+const counter = document.querySelector("p");
+const decreaseBtn = document.querySelector(".decrease");
+const increaseBtn = document.querySelector(".increase");
+const resetBtn = document.querySelector(".reset");
 
-
-decreaseBtn.addEventListener('click', ()=>{
-      counter.innerText-=1
-      })
-increaseBtn.addEventListener('click', ()=>{
-       counter.innerText=+counter.innerText+1
-      })
-resetBtn.addEventListener('click', ()=>{
-       counter.innerText=0
-      })
+decreaseBtn.addEventListener("click", () => {
+  if (counter.innerText <= 0) {
+    decreaseBtn.style.cursor = "not-allowed";
+    return;
+  }
+  counter.innerText -= 1;
+});
+increaseBtn.addEventListener("click", () => {
+  if (+counter.innerText >= 0) decreaseBtn.style.cursor = "pointer";
+  counter.innerText = +counter.innerText + 1;
+});
+resetBtn.addEventListener("click", () => {
+  counter.innerText = 0;
+});
