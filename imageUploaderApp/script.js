cf517622b537e8ffb8bab8cd8276d2252e18860e
Haykuhie <haykuhiek@gmail.com>
2021-12-02 13:09:43 +0400

    image uploader app created

---
+++ imageUploaderApp/script.js
@@ -0,0 +1,13 @@
+const inputField= document.querySelector('input')
+const imageField= document.querySelector('.imageContainer')
+var uploadedImage = ''
+
+
+inputField.addEventListener('change', function(){
+        const reader = new FileReader();        
+        reader.addEventListener('load', ()=>{
+            uploadedImage=reader.result;
+            imageField.style.backgroundImage= `url(${uploadedImage})`
+                })
+        reader.readAsDataURL(this.files[0])    
+})
