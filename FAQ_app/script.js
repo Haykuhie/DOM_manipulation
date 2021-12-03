3dafbcbc68a6cf97ca84e13b9775b61bbf2d97b9
Haykuhie <haykuhiek@gmail.com>
2021-12-03 21:50:50 +0400

    a bug fixed in FAQ_app(li-s not closing when opened the others)

--- FAQ_app/script.js
+++ FAQ_app/script.js
@@ -1,5 +1,6 @@
 const questions = document.querySelectorAll("li");
 const answers = document.querySelectorAll(".answerItem");
+const plusSigns = document.querySelectorAll(".forPlus");
 
 questions.forEach(function (listItem) {
   listItem.addEventListener("click", (event) => {
@@ -11,9 +12,16 @@ questions.forEach(function (listItem) {
     let plusSign = liNode.firstElementChild.lastElementChild;
     let answer = liNode.lastElementChild;
 
+    for (let i = 0; i < answers.length; i++) {
+      if (answers[i] == answer) continue;
+      answers[i].classList.remove("visible");
+      plusSigns[i].innerText = "+";
+    }
+
     answer.classList.toggle("visible");
-    if (plusSign.innerText == "+") {
-      plusSign.innerText = "−";
+
+    if (answer.classList.contains("visible")) {
+      plusSign.innerText = "–";
     } else {
       plusSign.innerText = "+";
     }
